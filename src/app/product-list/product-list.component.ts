943a1492aa1e8f70e4cd5b9a6fac9b217ae579d7
Billy Huang <10646003@ntub.edu.tw>
2020-07-26 02:27:40 +0800

    feat(product-list): init

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../../fixtures/products';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products = products;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  share(): void {
+    window.alert('The product has been shared!');
   }
 
 }
