fc1b6ad38a9a0ebbedce389a7d2beb7cb18a27de
Billy Huang <10646003@ntub.edu.tw>
2020-07-26 13:09:46 +0800

    feat(cart): 表單

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Product } from '../model/product';
 import { CartService } from '../services/cart.service';
 
@@ -10,13 +11,28 @@ import { CartService } from '../services/cart.service';
 export class CartComponent implements OnInit {
 
   items: Product[] = [];
+  checkForm: FormGroup;
 
   constructor(
     private cartService: CartService,
-  ) { }
+    private formBuilder: FormBuilder,
+  ) {
+    this.checkForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData: object): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkForm.reset();
+
+    console.log('Your order has been submitted', customerData);
   }
 
 }
