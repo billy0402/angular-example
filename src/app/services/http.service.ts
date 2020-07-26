1ce0094932ee56945677d10f5d17302631a49971
Billy Huang <10646003@ntub.edu.tw>
2020-07-26 11:58:00 +0800

    feat(http): init

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getData<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
+  }
+
+  // tslint:disable-next-line:no-any
+  postData<T>(url: string, data: any): Observable<T> {
+    return this.http.post<T>(url, data);
+  }
+
 }
